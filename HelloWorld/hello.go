50fcb0b1392a4b0acb6faf5ccae90382ef0d3b83
Rasite <hrasiter@gmail.com>
2021-05-02 19:40:57 +0300

    refactoring hello world!

--- HelloWorld/hello.go
+++ HelloWorld/hello.go
@@ -3,14 +3,34 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const defaultSuffix = "World"
+const spanish = "Spanish"
+const french = "French"
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
+
 	if name == "" {
-		return englishHelloPrefix + "World"
+		name = defaultSuffix
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", ""))
 }
--- HelloWorld/hello_test.go
+++ HelloWorld/hello_test.go
@@ -11,17 +11,45 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("Hello, with name", func(t *testing.T) {
-		got := Hello("Chriss")
+		got := Hello("Chriss", "")
 		want := "Hello, Chriss"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Hello without name", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
 
+	})
+
+	t.Run("In Spanish", func(t *testing.T) {
+		got := Hello("Elanor", "Spanish")
+		want := "Hola, Elanor"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("In Spanish, without name", func(t *testing.T) {
+		got := Hello("", "Spanish")
+		want := "Hola, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("In French", func(t *testing.T) {
+		got := Hello("Henry", "French")
+		want := "Bonjour, Henry"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("In French, without name", func(t *testing.T) {
+		got := Hello("", "French")
+		want := "Bonjour, World"
+
+		assertCorrectMessage(t, got, want)
 	})
 
 }
