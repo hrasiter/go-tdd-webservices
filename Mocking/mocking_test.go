b330b560582fea86e4014d9b201d15518888bf40
Rasite <hrasiter@gmail.com>
2021-05-05 23:12:06 +0300

    test sleep sequence

--- Mocking/mocking_test.go
+++ Mocking/mocking_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
@@ -13,22 +14,58 @@ func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
 
-func TestCountDown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	sleeper := &SpySleeper{}
-	CountDown(buffer, sleeper)
+type CountDownOperationsSpy struct {
+	Calls []string
+}
 
-	got := buffer.String()
-	want := `3
+func (c *CountDownOperationsSpy) Sleep() {
+	c.Calls = append(c.Calls, sleep)
+}
+
+func (c *CountDownOperationsSpy) Write(p []byte) (n int, er error) {
+	c.Calls = append(c.Calls, write)
+	return
+}
+
+const sleep = "sleep"
+const write = "write"
+
+func TestCountDown(t *testing.T) {
+
+	t.Run("counts Sleep calls", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+
+		CountDown(buffer, &CountDownOperationsSpy{})
+
+		got := buffer.String()
+		want := `3
 2
 1
 Go!`
 
-	if got != want {
-		t.Errorf("want: %q, got: %q", want, got)
-	}
+		if got != want {
+			t.Errorf("want: %q, got: %q", want, got)
+		}
+	})
 
-	if sleeper.Calls != 4 {
-		t.Errorf("sleeper should be called 4 times! but called: %d", sleeper.Calls)
-	}
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepCounter := &CountDownOperationsSpy{}
+		CountDown(spySleepCounter, spySleepCounter)
+
+		want := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepCounter.Calls) {
+			t.Errorf("wanted calls: %v, got %v", want, spySleepCounter.Calls)
+		}
+	})
+
 }
